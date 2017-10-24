0b545ad4a0e782a8db916dde226e548bf3fe3eaa
William Brooks <brooks.william@gmail.com>
2017-10-24 12:35:11 -0700

    skeleton files

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+    
+// This function handles events where the search button
+$("#add-employee").on("click", function(event) {
+    event.preventDefault();
+    
+});
+
+
+function addEmployee() {
+
+    var tableRow = $("<tr>");
+
+    var tableDataString = "<td>" +  $("") < /td> "
+                         + " <td > + < /td> <td > 10 / 10 / 2015 < /td> <
+    td > 100 < /td> <
+    td > $100 < /td> <
+    td > $1000 < /td>
+
+
+}
+
